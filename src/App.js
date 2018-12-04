4b40d15cd512967504988acbe48d5452c42293bb
DRooke <thodl15@gmail.com>
2018-12-04 09:32:30 -0500

    Added initial files to begin constructing the application.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div id={"app"}>
+                
+            </div>
+        )
+    }
+}
+
+export default App;
