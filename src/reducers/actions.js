8ca6764317122e9f1113aaf3e513cc5a3dbc9301
DRooke <thodl15@gmail.com>
2018-12-17 20:48:01 -0500

    Added new actions for adjusting card totals in the current decklist.

--- src/reducers/actions.js
+++ src/reducers/actions.js
@@ -1,9 +1,24 @@
 export const UPDATE_DECKLIST = 'UPDATE_DECKLIST';
-export const ADD_CARD = 'ADD_CARD';
+export const INC_CARD_COUNT = 'INC_CARD_COUNT';
+export const DEC_CARD_COUNT = 'DEC_CARD_COUNT';
 
 export const updateDecklist = (deck) => {
     return {
         type: UPDATE_DECKLIST,
         deck
+    }
+}
+
+export const incrementCardCount = id => {
+    return {
+        type: INC_CARD_COUNT,
+        id
+    }
+}
+
+export const decrementCardCount = id => {
+    return {
+        type: DEC_CARD_COUNT,
+        id
     }
 }
