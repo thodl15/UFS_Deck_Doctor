541ee33e61eaa0a9cc5c20e002e3ff9d45d9a8cb
DRooke <thodl15@gmail.com>
2018-12-17 20:48:24 -0500

    Added reducer cases for the new increment/decrement actions.

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux';
 
-import { UPDATE_DECKLIST } from './actions';
+import { UPDATE_DECKLIST,
+         INC_CARD_COUNT,
+         DEC_CARD_COUNT } from './actions';
 
 function deck(state = [], action) {
     switch (action.type) {
         case UPDATE_DECKLIST:
             return action.deck
+        case INC_CARD_COUNT:
+            return state ? state.map( card => {
+                return card.id === action.id ? ({...card, count: Math.min(card.count + 1,4)}) : card
+            }) : []
+        case DEC_CARD_COUNT:
+            return state ? state.map( card => {
+                return card.id === action.id ? ({...card, count: Math.max(card.count - 1, 0)}) : card
+            }) : []
         default:
             return state
     }
