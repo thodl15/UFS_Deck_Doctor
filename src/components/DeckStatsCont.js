a67408b1c9c64de0d0b52d399301c1331a79de09
DRooke <thodl15@gmail.com>
2019-01-02 10:36:19 -0500

    Added logic to aggregate card totals for spam foundations in the deck.

--- src/components/DeckStatsCont.js
+++ src/components/DeckStatsCont.js
@@ -6,14 +6,28 @@ const mapStateToProps = state => ({
     deck: state.deck
 })
 
+
+// NOTE:
+// I'm pretty sure that I am misusing this function
+// by piggy-backing my own function into its object
+// definition, primarily because said function does
+// not call dispatch() whatsoever.
 const mapDispatchToProps = dispatch => ({
     getStats: deck => generateDeckStats(deck)
 })
 
 const generateDeckStats = deck => {
+    console.log(deck);
+    var blockZones = [];
+    var spamFoundations = deck.map(x => parseInt(x.diff) < 2 ? x.count : 0);
+    
+    if(deck.length > 0) {
+        var deckSize = deck.reduce((total, card) => parseInt(total) + parseInt(card.count), 0);
+    }
+    
     return deck.length === 0 ? [] :
     [
-        "Spam Foundations",
+        `Spam Foundations: ${(spamFoundations.reduce((total, num) => total + num)/deckSize * 100).toFixed(2)}%\n`,
         "High/Mid/Low Blocks"
     ]
 }
